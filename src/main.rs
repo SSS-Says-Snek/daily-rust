c02a5030833b694eb16c612bc8482b35e2c9dc36
Brandon <bmcomis2018@gmail.com>
2022-10-08 23:55:26 -0500

    finish first rust prog

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 use std::io;
 use std::io::Write;
+use std::cmp::Ordering;
 
 fn main() {
-    let mut name = String::new();
-    print!("Enter your name: ");
-    io::stdout().flush().unwrap();
+    const MY_AGE: i32 = 15;
 
-    io::stdin()
-        .read_line(&mut name)
-        .expect("L bozo");
-    
-    println!("Your name is {name}")
+    loop {
+        let mut input = String::new();
+        print!("Enter your age: ");
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("L bozo");
+        
+        if input.trim() == "quit" {
+            println!("Bye!");
+            break;
+        }
+
+        let age: i32 = input.trim().parse().expect("ENTER. A. NUMBER");
+        println!("You're {age} years old!");
+
+        match age.cmp(&MY_AGE) {
+            Ordering::Less => println!("You're younger than me!"),
+            Ordering::Greater => println!("You're older than me!"),
+            Ordering::Equal => {
+                println!("Hi");
+                break;
+            },
+        }
+    }
 }
