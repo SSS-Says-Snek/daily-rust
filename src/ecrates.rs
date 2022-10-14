7ab64a782f8ceb1d84210f1e0bbc215dec4196a1
Brandon <bmcomis2018@gmail.com>
2022-10-14 00:03:04 -0500

    Add colors

---
+++ src/ecrates.rs
@@ -0,0 +1,7 @@
+// Just me testing out some cool external crates
+
+use colored::Colorize;
+
+pub fn main() {
+    println!("{}", "hello world!".red());
+}
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,23 @@ mod rps;
 mod secret_message;
 mod taylor;
 mod utils;
+mod ecrates;
 
 use std::io::{self, Write};
+use colored::Colorize;
 
 fn main() {
-    println!("Test rust stuff\n");
+    println!("\n{}", "Test rust stuff\n".bright_yellow());
     println!(
-        "Progs to run: firstprog, fib, taylor, rects, bsort, secretmessage, deriv, product, rps"
+        "{} {}", "Progs to run:".bright_green(),
+        "firstprog, fib, taylor, rects, bsort, secretmessage, deriv, product, rps, ecrates".bright_cyan()
     );
 
     loop {
         // Am lazy to see if it's possible without, so I'll just match everything
         let mut progname = String::new();
 
-        print!("Enter prog to run: ");
+        print!("{}", "Enter prog to run: ".bright_green());
         io::stdout().flush().unwrap();
 
         io::stdin().read_line(&mut progname).expect("Input Error");
@@ -36,7 +39,8 @@ fn main() {
             "deriv" => deriv::main(),
             "product" => product::main(),
             "rps" => rps::main(),
-            _ => println!("Does not exist!"),
+            "ecrates" => ecrates::main(),
+            _ => println!("{}", "Does not exist!".bright_red()),
         }
     }
 }
